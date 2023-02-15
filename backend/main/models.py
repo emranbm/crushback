c3c5c3840e526444438a0ffcf4125732f71b3185
Emran Batmanghelich <emran.bm@gmail.com>
2023-02-16 02:06:14 +0330

    Feat[bot]: Save user on start

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,5 +8,9 @@ from django.db import models
 class User(AbstractUser):
     first_name = models.CharField(null=False, blank=True, default='', max_length=64)
     last_name = models.CharField(null=False, blank=True, default='', max_length=64)
-    telegram_username = models.CharField(max_length=32)
-    telegram_chat_id = models.BigIntegerField()
+    telegram_username = models.CharField(null=False, blank=True, default='', max_length=32)
+    telegram_chat_id = models.BigIntegerField(null=True)
+
+    @staticmethod
+    def generate_random_username() -> str:
+        ''.join(random.choice(string.ascii_letters) for i in range(64))
--- backend/main/telegrambot/bot.py
+++ backend/main/telegrambot/bot.py
@@ -1,5 +1,11 @@
+import random
+import string
+
+from asgiref.sync import sync_to_async
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+from main import models
 
 
 class TelegramBot:
@@ -11,13 +17,12 @@ class TelegramBot:
                 .proxy_url(proxy_url) \
                 .get_updates_proxy_url(proxy_url)
         app = app_builder.build()
-        app.add_handler(CommandHandler('start', _Commands.start))
+        app.add_handler(CommandHandler('start', TelegramBot._on_start))
         app.run_polling()
 
-
-class _Commands:
     @staticmethod
-    async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def _on_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await TelegramBot._create_or_update_user(update)
         await context.bot.send_message(chat_id=update.effective_chat.id,
                                        text=f'''
 Hello {update.effective_user.first_name},
@@ -26,3 +31,18 @@ I'm newborn and still under construction...
 Would you please text me later?
 I'll love checking if your crush is already having a crushback on you!
         ''')
+
+    @staticmethod
+    async def _create_or_update_user(update: Update) -> models.User:
+        (user, created) = await models.User.objects.aget_or_create(
+            telegram_chat_id=update.effective_chat.id,
+            defaults={
+                'telegram_username': update.effective_user.username,
+                'username': ''.join(random.choice(string.ascii_letters) for i in range(64)),
+                'first_name': update.effective_user.first_name,
+                'last_name': update.effective_user.last_name or '',
+            })
+        if not created and user.telegram_username != update.effective_user.username:
+            user.telegram_username = update.effective_user.username
+            await sync_to_async(user.save)()
+        return user
