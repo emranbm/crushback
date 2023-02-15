b06ab2d5b1b05d54f9de7dc166f3bf6748e2cdae
Emran Batmanghelich <emran.bm@gmail.com>
2023-02-15 23:48:55 +0330

    Refactor[bot]: Take token as --token

--- backend/main/management/commands/telegrambot.py
+++ backend/main/management/commands/telegrambot.py
@@ -7,12 +7,13 @@ class Command(BaseCommand):
     help = 'Manages Telegram bot agent'
 
     def add_arguments(self, parser):
-        parser.add_argument('bot_token', type=str)
+        parser.add_argument('--token', action='store',
+                            help="Telegram bot token.", required=True)
         parser.add_argument('--proxy', action='store',
                             help="Optional proxy URL to be used. e.g. socks5://127.0.0.1:9000")
 
     def handle(self, *args, **options):
-        token = options['bot_token']
+        token = options['token']
         proxy_url = options.get('proxy')
         self.stdout.write("Running the bot...")
         TelegramBot.run(token, proxy_url)
