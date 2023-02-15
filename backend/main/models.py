f0505d5728da778744c6399e717c6037892a39c2
Emran Batmanghelich <emran.bm@gmail.com>
2023-02-16 00:08:41 +0330

    Refactor[back]: Clean models

--- backend/main/models.py
+++ backend/main/models.py
@@ -2,14 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=128)
-    description = models.TextField()
-    price = models.PositiveIntegerField()
-    isBuyRequest = models.BooleanField(default=False)
-
-
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=len("+989120001122"))
     first_name = models.CharField(null=False, blank=True, default='', max_length=64)
     last_name = models.CharField(null=False, blank=True, default='', max_length=64)
+    telegram_username = models.CharField(max_length=32)
+    telegram_chat_id = models.BigIntegerField()
