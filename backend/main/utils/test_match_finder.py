0c262eeca73d79b643d903c5990438174b26d60a
Emran Batmanghelich <emran.bm@gmail.com>
2023-02-25 02:05:02 +0330

    Test[back]: Add more tests for match finder

--- backend/main/utils/test_match_finder.py
+++ backend/main/utils/test_match_finder.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.test import TestCase
 
 from main.models import User, Crush
@@ -9,10 +11,34 @@ class MatchFinderTest(TestCase):
         new_matches = MatchFinder.save_new_matched_records()
         self.assertEqual(0, len(new_matches))
 
-    def test_users_get_matched_when_adding_each_other_telegram_usernames(self):
-        user1 = User.objects.create_user("test1", telegram_username="tg1")
-        user2 = User.objects.create_user("test2", telegram_username="tg2")
-        Crush(telegram_username="tg2", crusher=user1).save()
-        Crush(telegram_username="tg1", crusher=user2).save()
+    def _create_user_and_their_crush(self, self_telegram_username: str, crush_telegram_username: str) -> User:
+        user = User.objects.create_user(f"{self_telegram_username}_local", telegram_username=self_telegram_username)
+        Crush(telegram_username=crush_telegram_username, crusher=user).save()
+        return user
+
+    def test_users_get_matched_when_adding_each_others_telegram_usernames(self):
+        self._create_user_and_their_crush("tg1", "tg2")
+        self._create_user_and_their_crush("tg2", "tg1")
         new_matches = MatchFinder.save_new_matched_records()
         self.assertEqual(1, len(new_matches))
+
+    def test_should_only_find_new_matches(self):
+        self._create_user_and_their_crush("tg1", "tg2")
+        self._create_user_and_their_crush("tg2", "tg1")
+        new_matches = MatchFinder.save_new_matched_records()
+        self.assertEqual(1, len(new_matches))
+        new_matches = MatchFinder.save_new_matched_records()
+        self.assertEqual(0, len(new_matches))
+
+    def test_should_not_match_with_different_crushes(self):
+        self._create_user_and_their_crush("tg1", "crush1")
+        self._create_user_and_their_crush("tg2", "crush2")
+        new_matches = MatchFinder.save_new_matched_records()
+        self.assertEqual(0, len(new_matches))
+
+    @skip("Feature not completed")
+    def test_should_not_match_without_crushback(self):
+        self._create_user_and_their_crush("tg1", "crush1")
+        self._create_user_and_their_crush("crush1", "crush2")
+        new_matches = MatchFinder.save_new_matched_records()
+        self.assertEqual(0, len(new_matches))
