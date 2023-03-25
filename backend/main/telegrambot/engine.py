a7c2ba48078f67fb75f633c8ad6cb4befad8d684
Emran Batmanghelich <emran.bm@gmail.com>
2023-03-25 17:10:23 +0330

    Fix[bot]: Add delcrush handler

--- backend/main/telegrambot/engine.py
+++ backend/main/telegrambot/engine.py
@@ -4,6 +4,7 @@ from telegram.ext import ApplicationBuilder, Application
 from main.telegrambot.command_handlers.list_crushes_handler import ListCrushesHandler
 from main.telegrambot.command_handlers.start_command_handler import StartCommandHandler
 from main.telegrambot.conversation_handlers.addcrush_handler import AddcrushHandler
+from main.telegrambot.conversation_handlers.delcrush_handler import DelcrushHandler
 
 
 class TelegramBotEngine:
@@ -22,5 +23,6 @@ class TelegramBotEngine:
         app = TelegramBotEngine.create_app()
         app.add_handler(StartCommandHandler())
         app.add_handler(AddcrushHandler())
+        app.add_handler(DelcrushHandler())
         app.add_handler(ListCrushesHandler())
         app.run_polling(drop_pending_updates=True)
