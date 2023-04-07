10c6cf0cf05cfaad27c92a259ac247dab3da09d4
Emran Batmanghelich <emran.bm@gmail.com>
2023-04-07 17:55:32 +0330

    CI: Make restore job use Dropbox API

--- .github/scripts/restore-backup.sh
+++ .github/scripts/restore-backup.sh
@@ -5,18 +5,14 @@ readonly CWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 readonly ROOT_DIR="$CWD/../.."
 
 readonly CRUSHBACK_DB_PASSWORD="$1"
-readonly BACKUP_FILE_URL="$2"
-
-if [[ "$BACKUP_FILE_URL" == *\?* ]]; then
-  echo "ERROR: URL should not contain query params!"
-  echo "Hint: If it's forcefully required, change the script to tolerate '?' in the input URL."
-  exit 1
-fi
+readonly DROPBOX_TOKEN="$2"
+readonly BACKUP_FILENAME="$3"
 
 restore_job_manifest=$(helm template "$ROOT_DIR/k8s/helm/crushback-backend" --name-template crushback-backend \
   --set backup.restore.create=true \
-  --set backup.restore.backupFileURL="$BACKUP_FILE_URL" \
   --set database.password="$CRUSHBACK_DB_PASSWORD" \
+  --set backup.dropboxToken="$DROPBOX_TOKEN" \
+  --set backup.restore.backupFilename="$BACKUP_FILENAME" \
   --show-only templates/restore-job.yaml)
 
 echo "Deleting previous restore job, if exists ..."
